d0279b8e93528603ffd2a2d8080a72df38b74325
Thomas Magill <github@tmagill.net>
2016-09-20 20:36:12 -0700

    Removed credentials

--- routes.py
+++ routes.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
 from flask_mail import Message, Mail
-from mail_creds import *
+import mail_creds
 
-app.config["MAIL_SERVER"] = config["MAIL_SERVER"]
-app.config["MAIL_PORT"] = config["MAIL_PORT"]
-app.config["MAIL_USE_SSL"] = config["MAIL_USE_SSL"]
-app.config["MAIL_USERNAME"] = config["MAIL_USERNAME"]
-app.config["MAIL_PASSWORD"] = config["MAIL_PASSWORD"]
+
 
 mail = Mail()
 
@@ -15,7 +11,11 @@ app = Flask(__name__)
 
 app.secret_key = 'abcde12345'
 
-
+app.config["MAIL_SERVER"] = mail_creds.MAIL_SERVER
+app.config["MAIL_PORT"] = mail_creds.MAIL_PORT
+app.config["MAIL_USE_SSL"] = mail_creds.MAIL_USE_SSL
+app.config["MAIL_USERNAME"] = mail_creds.MAIL_USERNAME
+app.config["MAIL_PASSWORD"] = mail_creds.MAIL_PASSWORD
 
 mail.init_app(app)
 
@@ -39,8 +39,9 @@ def apply():
         %s
         """ % (form.name.data, form.email.data, form.pets.data)
         mail.send(msg)
+        print msg.body
 
-        return 'Form posted.'
+        return 'Your Application has been submitted.'
 
   elif request.method == 'GET':
     return render_template('apply.html', form=form)
