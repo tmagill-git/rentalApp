470bfb848a4e4917d3447298abf2663555ef9da1
Thomas Magill <github@tmagill.net>
2016-09-20 19:50:38 -0700

    Email Test

--- routes.py
+++ routes.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
- 
-app = Flask(__name__)      
+from flask_mail import Message, Mail
+
+mail = Mail()
+
+app = Flask(__name__)
 
 app.secret_key = 'abcde12345'
- 
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'mrthomasmagill@gmail.com'
+app.config["MAIL_PASSWORD"] = 'please38G'
+
+mail.init_app(app)
+
 @app.route('/')
 def home():
   return render_template('index.html')
@@ -12,15 +23,22 @@ def home():
 @app.route('/apply', methods=['GET', 'POST'])
 def apply():
   form = ContactForm()
- 
+
   if request.method == 'POST':
-    print form.validate()
     if form.validate() == False:
+        print form.validate()
         flash('All fields are required.')
         return render_template('apply.html', form=form)
-    else:    
+    else:
+        msg = Message("Test", sender='contact@example.com', recipients=['your_email@example.com'])
+        msg.body = """
+        From: %s <%s>
+        %s
+        """ % (form.name.data, form.email.data, form.pets.data)
+        mail.send(msg)
+
         return 'Form posted.'
- 
+
   elif request.method == 'GET':
     return render_template('apply.html', form=form)
 
