17da6a3cbdd25047a1281f13b10e4088f19bf4e9
Thomas Magill <github@tmagill.net>
2016-09-21 08:09:41 -0700

    Moved to tcp/80

--- routes.py
+++ routes.py
@@ -93,4 +93,4 @@ def apply():
     return render_template('apply.html', form=form)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0', port=80)
